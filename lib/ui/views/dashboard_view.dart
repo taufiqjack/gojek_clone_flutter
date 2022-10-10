4bdcc8a27ebb3cdc3cd0ac1dffc0b099e6a3e675
taufiqjack <taufiqdwic@gmail.com>
2022-10-10 15:33:07 +0700

    update banner gopay

--- lib/ui/views/dashboard_view.dart
+++ lib/ui/views/dashboard_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gojek_ui_flutter/core/constans/colors_custom.dart';
+import 'package:gojek_ui_flutter/main.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({super.key});
@@ -25,7 +27,8 @@ class _DashboardViewState extends State<DashboardView> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   Container(
-                    height: 25,
+                    height: 30,
+                    width: 75,
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(20),
                         color: colorwhite),
@@ -54,9 +57,121 @@ class _DashboardViewState extends State<DashboardView> {
           ),
           automaticallyImplyLeading: false,
           elevation: 0),
-      body: const Center(
-        child: Text('Gojek Indonesia'),
-      ),
+      body: Padding(
+          padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Flexible(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 5, right: 5),
+                      child: SizedBox(
+                        height: 50,
+                        child: Center(
+                          child: TextFormField(
+                            decoration: InputDecoration(
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30)),
+                                enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(30)),
+                                prefixIcon: InkWell(
+                                  onTap: () {},
+                                  child: const Icon(
+                                    Icons.search,
+                                  ),
+                                ),
+                                hintText: 'Cari layanan, makanan, & tujuan'),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: const NetworkImage(
+                        'https://yt3.ggpht.com/QsCVkOQB_ULkoXPHeUJAFANf3K51nGqa4w17LnJFCh8-EgS3ykqeRYbsmVS4f_jSOfnNgKk-3tM=s900-c-k-c0x00ffffff-no-rj'),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: SvgPicture.asset('assets/images/club.svg'),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                height: 100,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10), color: colorBlue),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 100,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: colorwhite),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 5),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Image.asset(
+                              'assets/images/gopay.png',
+                            ),
+                            const Text(
+                              'Rp10.000.000',
+                              style: TextStyle(fontWeight: FontWeight.w700),
+                            ),
+                            Text(
+                              'Klik & cek riwayat',
+                              style:
+                                  TextStyle(color: colorGreenTwo, fontSize: 12),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: colorwhite),
+                      child: Center(
+                        child: SvgPicture.asset('assets/images/up.svg'),
+                      ),
+                    ),
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: colorwhite),
+                      child: Center(
+                        child: SvgPicture.asset('assets/images/plus.svg'),
+                      ),
+                    ),
+                    Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(5),
+                          color: colorwhite),
+                      child: Center(
+                        child: Image.asset('assets/images/explore.png'),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          )),
     );
   }
 }
